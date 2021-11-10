3ce97b55b08ae9eb72c94e98077ab95d230d74d9
Joe Cserko <j.cserko@gmail.com>
2021-11-10 10:39:58 +0100

    sum

--- js3-intl-2021-11-08.js
+++ js3-intl-2021-11-08.js
@@ -46,6 +46,57 @@ const collectUsers = (list = [{users: 1}]) => {
 };
 // console.log( collectUsers([{users: 4}, {users: 5}, {users: 1}]) );
 
+// Feladat: írj egy függvényt, ami visszatér a `Szia, Pali vagyok!` string-el!
+const stringProvider = () => {
+    return 'Szia, Pali vagyok!';
+};
+
+// Fájl: app2.js fájlban dolgozz.
+// Függvény neve: calcAmount
+// Export: legyen benne a calcAmount függvény
+// _Adja vissza a kapott szám háromszorosát, osztva 5.5-el és kerekítve_.
+const calcAmount = (number = 0) => Math.round(number * 3 / 5.5);
+
+// Fájl: app3.js fájlban dolgozz.
+// Függvény neve: sumPoints
+// Export: legyen benne a sumPoints függvény
+// - A függvény objektumok tömbjét kapja.
+// - Minden objektumban van egy point nevű tulajdonság, ami number típusú.
+
+// - A függvény adja vissza a point-ok összegét.
+const sumPoints = (list = [{point: 5}, {point: 3}]) => {
+    // 1. Akkumulátor:
+    let allOfPoints = 0;
+
+    // 2. Bejárom a tömböt és hozzáadom a allOfPoints változóhoz a tömbelem price 
+    // tulajdonságának az értékét:
+    list.forEach( item => {
+        console.log(item);
+        allOfPoints += item.point;
+     } );
+
+    // Visszaadom az akkumulált (összegyűjtött) értéket:
+    return allOfPoints;
+};
+
+const players = [
+    {
+        name: 'Marika néni',
+        point: 121,
+    },
+    {
+        name: 'Vérpistike',
+        point: 9,
+    },
+    {
+        name: 'Gyúrós Pityu',
+        point: 120,
+    },
+];
+
+console.log(sumPoints(players));
+
+
 export {
-    collectUsers
+    sumPoints,
 };
